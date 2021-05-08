0e96bdef2cc3b699172e890f012a9e0a3f7efe20
Murat Polat <murat.polat@alumni.fh-aachen.de>
2021-05-08 16:35:51 +0200

    pushed Aufgabe 2&3

--- abalo/resources/views/Templates/base_template.blade.php
+++ abalo/resources/views/Templates/base_template.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta id="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous" defer></script>
